a4732c43bdad240ccec1e7e1b055093e56e4083d
Ashwini-2021 <80737225+Ashwini-2021@users.noreply.github.com>
2021-04-19 12:31:42 +0530

    Add files via upload
    star pattern printing

---
+++ 5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=4;j++)
+        {
+            if((j==1)&&(i==1))
+            {  printf(" * "); }
+           else if((j==2)&&(i<=2))
+            {  printf(" * "); }
+           else if((j==3)&&(i<=3))
+            {  printf(" * "); }
+           else if((j==4)&&(i<=4))
+            {  printf(" * "); }
+            else{
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/*OUTPUT
+ *  *  *  * 
+    *  *  * 
+       *  * 
+          *   */
